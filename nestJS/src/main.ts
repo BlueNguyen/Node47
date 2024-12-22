ea1a9ede32eca7641ee83229407a0855a66184e8
BlueNguyen <143895447+BlueNguyen@users.noreply.github.com>
2024-12-22 19:13:18 +0700

    modify

--- nestJS/src/main.ts
+++ nestJS/src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  const config= new DocumentBuilder().setTitle("NestJS Swagger").addBearerAuth().build()
+  const config= new DocumentBuilder().setTitle("NestJS Swagger (Ngoan Nguyen)").addBearerAuth().build()
   const document= SwaggerModule.createDocument(app,config)
   SwaggerModule.setup("/swagger", app,document)
   await app.listen(8080);
