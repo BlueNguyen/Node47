4dc2b2104fe973a7a4817b60e8d12f76fba3d70b
BlueNguyen <143895447+BlueNguyen@users.noreply.github.com>
2024-12-23 21:48:37 +0700

    modify

--- nestJS/src/main.ts
+++ nestJS/src/main.ts
@@ -4,10 +4,21 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
-  const config= new DocumentBuilder().setTitle("NestJS Swagger (Ngoan Nguyen)").addBearerAuth().build()
-  const document= SwaggerModule.createDocument(app,config)
-  SwaggerModule.setup("/swagger", app,document)
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Swagger (Ngoan Nguyen)')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  // Cấu hình CORS
+  app.enableCors({
+    origin: '*', // Cho phép tất cả nguồn gốc. Thay '*' bằng domain cụ thể nếu cần.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(8080);
 }
 bootstrap();
